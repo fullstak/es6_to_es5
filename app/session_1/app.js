9b93628cc20192e36b95acd521da2ced503e5ede
abhinandan <smbishwakarma@gmail.com>
2017-02-25 14:30:08 +0530

    Session 1 Change

---
+++ app/session_1/app.js
@@ -0,0 +1,58 @@
+/*let name = "Suraj";
+let age = 20;
+//Key Value pair es5
+var obj = {name: "suraj", age: 10};
+
+let object = {name,age};
+
+console.log(object)*/
+
+//Conditions
+
+var a = 11;
+/*
+if(a == 10){
+    console.log("It's 10");
+}
+else if(a == 20){
+    console.log("It's 20");
+}
+else{
+    console.log("It's not 10");
+}*/
+//Node condition
+/*if(a != 10){
+    console.log("a is not 10")
+}*/
+/*
+var result = (a == 10) ? "It's 10" : "It's not 10";
+console.log(result)*/
+
+
+// Operators and Conditions
+
+var myarray = [10,3,2,6,4,8,1,7,3,7,8,9];
+
+//Loops for, while, dowhile forEach is
+
+//for(assignment/init;comprasion;increment/decrement)
+//global and local
+
+var k = 0;
+
+/*if(myarray[1] == 10){
+    console.log(11)
+}
+
+for(let i = 0;i<10;i++){
+    console.log("Value ",i);
+    console.log("Executed");
+}
+
+console.log("End ", i);*/
+
+
+
+
+
+
